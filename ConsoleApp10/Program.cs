ebb398adb44ebcf4021c27281d643e01013c0913
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-04-27 16:38:10 +0530

    IList

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp10
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        IList<int> intList = new List<int>() { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+
+        Console.WriteLine(intList.Count);
+
+        IList<Student> studentList = new List<Student>() {
+                new Student(){ StudentID=1, StudentName="Amol"},
+                new Student(){ StudentID=2, StudentName="Anil"},
+                new Student(){ StudentID=3, StudentName="Rahul"},
+                new Student(){ StudentID=4, StudentName="Vishaal"}
+            };
+
+        Console.WriteLine(studentList.Count);
+
     }
+}
+
+public class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+
 }
